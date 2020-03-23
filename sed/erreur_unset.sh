f70672ce56dcc20267b472eb4e1df0dd2e92274b
nassafou <ntyoussouf@gmail.com>
2020-03-23 07:57:30 -0700

    test

---
+++ sed/erreur_unset.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+#set -o nounset 
+
+if [ -z "$Chaine" ] ; then
+  echo "La chaine est vide "
+fi
+
---
+++ sed/exemple_exit.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+verifie_fichier ()
+{
+  if [ ! -f $1 ]; then
+      echo "$1 n'existe pas !"
+      exit 
+  fi
+}
+
+if [ $# -ne 1 ]; then 
+    echo "Syntaxe : $0 fichier"
+    exit 1
+fi
+
+verifie_fichier $1
+file $1
+exit 0
